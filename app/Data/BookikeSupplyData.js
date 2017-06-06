a76e1902f5c9221f824ed5cc049f05d125f02a1a
Jared <kuil5230@gmail.com>
2017-06-07 00:24:28 +0800

    ci

---
+++ app/Data/BookikeSupplyData.js
@@ -0,0 +1,136 @@
+module.exports = [
+    {
+        Line: "01",
+        Year: "溪資29630",
+        RightPerson: "王瑞萌",
+        DuringTheAnnouncementPeriod: "106-06-06~106-07-06",
+        Lot: "鳳山",
+        LandmarkKeyNumber: '459',
+        BookNumber: '76彰溪字第1121號'
+    }, {
+        Line: "02",
+        Year: "106溪資29230",
+        RightPerson: "黃淑嬌",
+        DuringTheAnnouncementPeriod: "106-06-05~106-07-05",
+        Lot: "西寮",
+        LandmarkKeyNumber: '227,241',
+        BookNumber: '78彰溪字第3999,9163號'
+    },
+    {
+        Line: "03",
+        Year: "106溪資28790",
+        RightPerson: "陳有添",
+        DuringTheAnnouncementPeriod: "106-06-03~106-07-03",
+        Lot: "新東榮段",
+        LandmarkKeyNumber: '486',
+        BookNumber: '104溪資字第11476號'
+    },
+    {
+        Line: "04",
+        Year: "106溪資28720",
+        RightPerson: "胡毓泉",
+        DuringTheAnnouncementPeriod: "106-06-02~106-07-03",
+        Lot: "鳳山段.光平段",
+        LandmarkKeyNumber: '1244,153',
+        BookNumber: '75彰溪字第14198,14169號'
+    },
+    {
+        Line: "05",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "06",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "07",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "08",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "09",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "10",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "11",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "12",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "13",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "14",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+    {
+        Line: "15",
+        Year: "",
+        RightPerson: "",
+        DuringTheAnnouncementPeriod: "",
+        Lot: "",
+        LandmarkKeyNumber: '',
+        BookNumber: ''
+    },
+];
--- app/page/Proclamation/BooklikeSupply.js
+++ app/page/Proclamation/BooklikeSupply.js
@@ -6,12 +6,73 @@
 
 //書狀補給
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ListView, Text, View } from 'react-native';
+
+import BooklikeSupplyData from '../../Data/BookikeSupplyData';
 
 export default class BooklikeSupply extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            })
+        };
+    }
+
+    componentWillMount() {
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(BooklikeSupplyData)
+        });
+    }
+
     render() {
         return (
             <View style={ styles.container }>
+            </View>
+            );
+    }
+
+
+    render() {
+        return (
+            <View style={ styles.container }>
+              <ListView dataSource={ this.state.dataSource } pageSize={ 1 } initialListSize={ 1 } renderRow={ this.renderRow.bind(this) } />
+            </View>
+            );
+    }
+
+    renderRow(msg) {
+        return (
+            <View style={ { width: '100%', marginBottom: 10, backgroundColor: '#e0e0e0' } }>
+              <Text>
+                項次:
+                { msg.Line }
+              </Text>
+              <Text>
+                年 字 號:
+                { msg.Year }
+              </Text>
+              <Text>
+                權利人:
+                { msg.RightPerson }
+              </Text>
+              <Text>
+                公告期間:
+                { msg.DuringTheAnnouncementPeriod }
+              </Text>
+              <Text>
+                地段:
+                { msg.Lot }
+              </Text>
+              <Text>
+                地號建號:
+                { msg.LandmarkKeyNumber }
+              </Text>
+              <Text>
+                書狀號:
+                { msg.BookNumber }
+              </Text>
             </View>
             );
     }
@@ -21,7 +82,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF'
     }
 });
